5a1db4c5bf2efc627fd9de3ffacfd6d668966b1b
Josh Huelsman <joshuahuelsman@gmail.com>
2015-07-19 01:58:03 -0400

    Added NeHe tutorial examples 01 to 05.

---
+++ examples/nehe/lesson02/source/main.cpp
@@ -0,0 +1,86 @@
+/****************************************
+ *       3DS NeHe Lesson 02            *
+ *       Author: machinamentum         *
+ ****************************************/
+
+#include <3ds.h>
+#include <cmath>
+#include <GL/gl.h>
+#include <gfx_device.h>
+
+
+extern "C" {
+    extern void glFrustumf (GLfloat left, GLfloat right, GLfloat bottom, GLfloat top, GLfloat zNear, GLfloat zFar);
+}
+
+void DrawGLScene() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glLoadIdentity();
+    glTranslatef(-1.5f,0.0f,-6.0f);
+    glBegin(GL_TRIANGLES);                      // Drawing Using Triangles
+        glVertex3f( 0.0f, 1.0f, 0.0f);              // Top
+        glVertex3f(-1.0f,-1.0f, 0.0f);              // Bottom Left
+        glVertex3f( 1.0f,-1.0f, 0.0f);              // Bottom Right
+    glEnd();                            // Finished Drawing The Triangle
+
+    glTranslatef(3.0f,0.0f,0.0f);
+    glBegin(GL_QUADS);                      // Draw A Quad
+        glVertex3f(-1.0f, 1.0f, 0.0f);              // Top Left
+        glVertex3f( 1.0f, 1.0f, 0.0f);              // Top Right
+        glVertex3f( 1.0f,-1.0f, 0.0f);              // Bottom Right
+        glVertex3f(-1.0f,-1.0f, 0.0f);              // Bottom Left
+    glEnd();                            // Done Drawing The Quad
+
+}
+
+int main()
+{
+    gfxInitDefault();
+    hidInit(NULL);
+
+    void* device = gfxCreateDevice(240, 400);
+    gfxMakeCurrent(device);
+
+    glViewport(0, 0, 240, 400);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    float near = 0.1f;
+    float far = 100.0f;
+    float fov = 90.0f;
+    float aspect = 240.0f / 400.0f;
+    float t = tan(fov * 3.14159 / 360.0) * near;
+    float b = -t;
+    float l = aspect * b;
+    float r = aspect * t;
+    glFrustumf(l, r, b, t, near, far);
+    //3DS' framebuffers are sideways
+    glRotatef(-90.0f, 0.0f, 0.0f, 1.0f);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+
+    while (aptMainLoop())
+    {
+        hidScanInput();
+
+        if (keysDown() & KEY_START)
+            break;
+
+        DrawGLScene();
+
+        gfxFlush(gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL));
+        gfxFlushBuffers();
+        gfxSwapBuffersGpu();
+        gspWaitForVBlank();
+    }
+    
+    // Exit services
+    gfxExit();
+    hidExit();
+    return 0;
+}
---
+++ examples/nehe/lesson05/source/main.cpp
@@ -0,0 +1,160 @@
+/****************************************
+ *       3DS NeHe Lesson 05            *
+ *       Author: machinamentum         *
+ ****************************************/
+
+#include <3ds.h>
+#include <cmath>
+#include <GL/gl.h>
+#include <gfx_device.h>
+
+
+extern "C" {
+    extern void glFrustumf (GLfloat left, GLfloat right, GLfloat bottom, GLfloat top, GLfloat zNear, GLfloat zFar);
+}
+
+GLfloat     rtri;                       // Angle For The Triangle ( NEW )
+GLfloat     rquad;                      // Angle For The Quad     ( NEW )
+
+void DrawGLScene() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear The Screen And The Depth Buffer
+    glLoadIdentity();                   // Reset The View
+    glTranslatef(-1.5f,0.0f,-6.0f);             // Move Left And Into The Screen
+
+    glRotatef(rtri,0.0f,1.0f,0.0f);             // Rotate The Pyramid On It's Y Axis
+
+    glBegin(GL_TRIANGLES);                  // Start Drawing The Pyramid
+        glColor3f(1.0f,0.0f,0.0f);          // Red
+        glVertex3f( 0.0f, 1.0f, 0.0f);          // Top Of Triangle (Front)
+        glColor3f(0.0f,1.0f,0.0f);          // Green
+        glVertex3f(-1.0f,-1.0f, 1.0f);          // Left Of Triangle (Front)
+        glColor3f(0.0f,0.0f,1.0f);          // Blue
+        glVertex3f( 1.0f,-1.0f, 1.0f);          // Right Of Triangle (Front)
+
+        glColor3f(1.0f,0.0f,0.0f);          // Red
+        glVertex3f( 0.0f, 1.0f, 0.0f);          // Top Of Triangle (Front)
+        glColor3f(0.0f,1.0f,0.0f);          // Green
+        glVertex3f(-1.0f,-1.0f, 1.0f);          // Left Of Triangle (Front)
+        glColor3f(0.0f,0.0f,1.0f);          // Blue
+        glVertex3f( 1.0f,-1.0f, 1.0f);          // Right Of Triangle (Front)
+
+        glColor3f(1.0f,0.0f,0.0f);          // Red
+        glVertex3f( 0.0f, 1.0f, 0.0f);          // Top Of Triangle (Right)
+        glColor3f(0.0f,0.0f,1.0f);          // Blue
+        glVertex3f( 1.0f,-1.0f, 1.0f);          // Left Of Triangle (Right)
+        glColor3f(0.0f,1.0f,0.0f);          // Green
+        glVertex3f( 1.0f,-1.0f, -1.0f);         // Right Of Triangle (Right)
+
+        glColor3f(1.0f,0.0f,0.0f);          // Red
+        glVertex3f( 0.0f, 1.0f, 0.0f);          // Top Of Triangle (Back)
+        glColor3f(0.0f,1.0f,0.0f);          // Green
+        glVertex3f( 1.0f,-1.0f, -1.0f);         // Left Of Triangle (Back)
+        glColor3f(0.0f,0.0f,1.0f);          // Blue
+        glVertex3f(-1.0f,-1.0f, -1.0f);         // Right Of Triangle (Back)
+
+        glColor3f(1.0f,0.0f,0.0f);          // Red
+        glVertex3f( 0.0f, 1.0f, 0.0f);          // Top Of Triangle (Left)
+        glColor3f(0.0f,0.0f,1.0f);          // Blue
+        glVertex3f(-1.0f,-1.0f,-1.0f);          // Left Of Triangle (Left)
+        glColor3f(0.0f,1.0f,0.0f);          // Green
+        glVertex3f(-1.0f,-1.0f, 1.0f);          // Right Of Triangle (Left)
+    glEnd();                        // Done Drawing The Pyramid
+
+
+    glLoadIdentity();
+    glTranslatef(1.5f,0.0f,-7.0f);              // Move Right And Into The Screen
+
+    glRotatef(rquad,1.0f,1.0f,1.0f);            // Rotate The Cube On X, Y & Z
+
+    glBegin(GL_QUADS);                  // Start Drawing The Cube
+        glColor3f(0.0f,1.0f,0.0f);          // Set The Color To Green
+        glVertex3f( 1.0f, 1.0f,-1.0f);          // Top Right Of The Quad (Top)
+        glVertex3f(-1.0f, 1.0f,-1.0f);          // Top Left Of The Quad (Top)
+        glVertex3f(-1.0f, 1.0f, 1.0f);          // Bottom Left Of The Quad (Top)
+        glVertex3f( 1.0f, 1.0f, 1.0f);          // Bottom Right Of The Quad (Top)
+
+        glColor3f(1.0f,0.5f,0.0f);          // Set The Color To Orange
+        glVertex3f( 1.0f,-1.0f, 1.0f);          // Top Right Of The Quad (Bottom)
+        glVertex3f(-1.0f,-1.0f, 1.0f);          // Top Left Of The Quad (Bottom)
+        glVertex3f(-1.0f,-1.0f,-1.0f);          // Bottom Left Of The Quad (Bottom)
+        glVertex3f( 1.0f,-1.0f,-1.0f);          // Bottom Right Of The Quad (Bottom)
+
+        glColor3f(1.0f,0.0f,0.0f);          // Set The Color To Red
+        glVertex3f( 1.0f, 1.0f, 1.0f);          // Top Right Of The Quad (Front)
+        glVertex3f(-1.0f, 1.0f, 1.0f);          // Top Left Of The Quad (Front)
+        glVertex3f(-1.0f,-1.0f, 1.0f);          // Bottom Left Of The Quad (Front)
+        glVertex3f( 1.0f,-1.0f, 1.0f);          // Bottom Right Of The Quad (Front)
+
+        glColor3f(1.0f,1.0f,0.0f);          // Set The Color To Yellow
+        glVertex3f( 1.0f,-1.0f,-1.0f);          // Bottom Left Of The Quad (Back)
+        glVertex3f(-1.0f,-1.0f,-1.0f);          // Bottom Right Of The Quad (Back)
+        glVertex3f(-1.0f, 1.0f,-1.0f);          // Top Right Of The Quad (Back)
+        glVertex3f( 1.0f, 1.0f,-1.0f);          // Top Left Of The Quad (Back)
+
+        glColor3f(0.0f,0.0f,1.0f);          // Set The Color To Blue
+        glVertex3f(-1.0f, 1.0f, 1.0f);          // Top Right Of The Quad (Left)
+        glVertex3f(-1.0f, 1.0f,-1.0f);          // Top Left Of The Quad (Left)
+        glVertex3f(-1.0f,-1.0f,-1.0f);          // Bottom Left Of The Quad (Left)
+        glVertex3f(-1.0f,-1.0f, 1.0f);          // Bottom Right Of The Quad (Left)
+
+        glColor3f(1.0f,0.0f,1.0f);          // Set The Color To Violet
+        glVertex3f( 1.0f, 1.0f,-1.0f);          // Top Right Of The Quad (Right)
+        glVertex3f( 1.0f, 1.0f, 1.0f);          // Top Left Of The Quad (Right)
+        glVertex3f( 1.0f,-1.0f, 1.0f);          // Bottom Left Of The Quad (Right)
+        glVertex3f( 1.0f,-1.0f,-1.0f);          // Bottom Right Of The Quad (Right)
+    glEnd();                        // Done Drawing The Quad
+
+    rtri+=0.2f;                     // Increase The Rotation Variable For The Triangle ( NEW )
+    rquad-=0.15f;                       // Decrease The Rotation Variable For The Quad     ( NEW )
+}
+
+int main()
+{
+    gfxInitDefault();
+    hidInit(NULL);
+
+    void* device = gfxCreateDevice(240, 400);
+    gfxMakeCurrent(device);
+
+    glViewport(0, 0, 240, 400);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    float near = 0.1f;
+    float far = 100.0f;
+    float fov = 90.0f;
+    float aspect = 240.0f / 400.0f;
+    float t = tan(fov * 3.14159 / 360.0) * near;
+    float b = -t;
+    float l = aspect * b;
+    float r = aspect * t;
+    glFrustumf(l, r, b, t, near, far);
+    //3DS' framebuffers are sideways
+    glRotatef(-90.0f, 0.0f, 0.0f, 1.0f);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+
+    while (aptMainLoop())
+    {
+        hidScanInput();
+
+        if (keysDown() & KEY_START)
+            break;
+
+        DrawGLScene();
+
+        gfxFlush(gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL));
+        gfxFlushBuffers();
+        gfxSwapBuffersGpu();
+        gspWaitForVBlank();
+    }
+
+    // Exit services
+    gfxExit();
+    hidExit();
+    return 0;
+}
